f9c2c6ea387fc1558e0a9b3eb393b0c24f6dbb78
salmankavanur <mpsalman2011@gmail.com>
2025-05-16 18:44:17 +0530

    minor update

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,18 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    // Add custom resolver for punycode
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        ...config.resolve.fallback,
+        punycode: false,  // Disable the Node.js punycode module
+      },
+    };
+    
+    return config;
+  },
 }
 
 export default nextConfig
