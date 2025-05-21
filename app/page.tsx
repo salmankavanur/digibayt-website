9df0896254ca1e21ff19a06ba22eb4a387ad552d
salmankavanur <mpsalman2011@gmail.com>
2025-05-21 06:55:18 +0530

    minor updates in home header

--- app/page.tsx
+++ app/page.tsx
@@ -44,11 +44,12 @@ export default function Home() {
         <div className="container flex h-16 items-center justify-between">
           <div className="flex items-center gap-2">
             {/* <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gradient-to-br from-primary to-purple-600"> */}
-               <div className="flex h-10 w-10 items-center justify-center rounded-full">
-               <Image src="/icons/digibayt-logo.png" alt="DigiBayt Logo" width={32} height={32} className="object-contain" />
+            <div className="flex h-10 w-10 items-center justify-center rounded-full">
+              <Image src="/icons/digibayt-logo.png" alt="DigiBayt Logo" width={32} height={32} className="object-contain" />
               {/* <Zap className="h-5 w-5 text-white" /> */}
             </div>
-            <span className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary to-purple-600">
+            {/* <span className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary to-purple-600"> */}
+            <span className="text-xl font-bold bg-clip-text text-black">
               DigiBayt
             </span>
           </div>
